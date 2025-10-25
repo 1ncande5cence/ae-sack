88446853ef4321d00ec8ddbc62bda7843f988d23
Zhechang Marinabay <zbz5352@psu.edu>
2025-10-25 11:16:12 -0400

    nginx n4 ready

---
+++ scripts/nginx/n4_restrict/copy_tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+# program preparation
+cp $1/scripts/nginx/n4_restrict/nginx.conf $2/../conf/nginx.conf
+
+
+# subgt generation
+cp $1/tools/subgt_addresslog_gen.py $2
+cp $1/subgt/nginx/subgt.json $2
+
+# oracle input
+cp -r $1/scripts/nginx/n4_restrict/input $2
+cp $1/scripts/nginx/n4_restrict/send_request_disable.py $2
+
+# result analysis 
+cp $1/scripts/nginx/report.py $2
