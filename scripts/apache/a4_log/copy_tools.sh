facce98c4617ebac14b2410b4b4b05fa50db1cb5
Zhechang Marinabay <zbz5352@psu.edu>
2025-10-26 17:22:07 -0400

    a4 ready

---
+++ scripts/apache/a4_log/copy_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# program preparation
+#cp $1/scripts/apache/a1_auth/httpd.conf /usr/local/apache2/conf/httpd.conf
+
+
+# subgt generation
+cp $1/tools/subgt_addresslog_gen.py $2
+cp $1/subgt/apache/subgt.json $2
+
+# oracle input
+cp -r $1/scripts/apache/a4_log/input $2
+cp $1/scripts/apache/a4_log/send_request.logging.py $2
+
+# result analysis 
+cp $1/scripts/apache/report.py $2
