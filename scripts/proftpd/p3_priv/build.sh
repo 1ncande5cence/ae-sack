de8a78d25a541d36d986014bd7a94e066272e091
Zhechang Marinabay <zbz5352@psu.edu>
2025-10-26 00:11:07 -0400

    p3 ready

--- scripts/proftpd/p3_priv/build.sh
+++ scripts/proftpd/p3_priv/build.sh
@@ -9,44 +9,45 @@ export SACK=/ae-sack
 # -------------------- build project with wllvm --------------------------------
 
 export CC=wllvm CXX=wllvm++ LLVM_COMPILER=clang CFLAGS="-g -O0" CXXFLAGS="-g -O0"
-
+make clean
 ./configure --enable-ctrls --with-modules=mod_ban
 make -j$(nproc)
 
 # -------------------- build flip binaries -----------------------------------
 
-mkdir bin_vsack_oracle3_user_priv 
-cp proftpd bin_vsack_oracle3_user_priv/
-cd bin_vsack_oracle3_user_priv 
+mkdir bin_oracle3_user_priv 
+cp proftpd bin_oracle3_user_priv/
+cd bin_oracle3_user_priv 
 extract-bc proftpd
 export EXTRA_LDFLAGS="-lcrypt -lc -ldl"
 mkdir -p ./log
 cp $SACK/scripts/proftpd/p3_priv/sack.conf ./log/
 cp $SACK/scripts/proftpd/p3_priv/ban_line.list ./log/
+cp $SACK/scripts/proftpd/p3_priv/ftpreq_STOR.py .
 $SACK/AFL/afl-clang-fast-indirect-flip proftpd.bc -o proftpd.fuzz $EXTRA_LDFLAGS
 
 # -------------------- prepare tools and environments --------------------------
 
-bash $SACK/tools/copy_tools.sh $SACK .
+bash $SACK/scripts/proftpd/p3_priv/copy_tools.sh $SACK .
 objdump -d ./proftpd.fuzz | grep ">:" > ./log/func_map
+python3 subgt_addresslog_gen.py ./subgt.json
 
-# -------------------- put your corpus here ------------------------------------
+# -------------------- corpus is copied through copy_tools.sh ------------------------------------
 
-# NOTE: put your corpus for next step!
-# mkdir corpus; 
-# cp <your testcases> corpus/
-
-# -------------------- do branch flipping --------------------------------------
+# -------------------- do substitution --------------------------------------
+# in bin_oracle3_user_priv folder
+# terminal 1
 # python3 ftpreq_STOR.py
 
+# terminal 2
 # export AFL_NO_AFFINITY=1
-# cd bin_vsack_oracle3_user_priv
+# export SACK=/ae-sack
+# (this -c path and the path in the proftpd.conf need to be absolute path)
 # $SACK/AFL/afl-fuzz -c ./log/sack.conf -m 100M -i ./input/ -o output/ -t 1000 -- ./proftpd.fuzz -n -c /methodology.new/proftpd-collection/proftpd/bin/proftpd.conf -d 5 -X
 
 
-# # -------------------- corruptibility assessment (auto) ------------------------
+# -------------------- result analysis --------------------------------------
 
-# # assess syscall-guard variables
-# python3 auto_rator.py ./sqlite3.bc ./dot/temp.dot br -- ./sqlite3_rate
-# # assess arguments of triggered syscalls
-# python3 auto_rator.py ./sqlite3.bc ./dot/temp.dot arg -- ./sqlite3_rate
+# use analyze.sh at the bin_safemode folder
+
+# the result is in the result.*/ folder report_unique.txt
---
+++ scripts/proftpd/p3_priv/copy_tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# program preparation
+
+cp $1/scripts/proftpd/p3_priv/proftpd.conf $2
+
+# subgt generation
+cp $1/tools/subgt_addresslog_gen.py $2
+cp $1/subgt/proftpd/subgt.json $2
+
+# oracle input
+cp -r $1/scripts/proftpd/p3_priv/input $2
+
+# result analysis 
+cp $1/scripts/proftpd/report.py $2
