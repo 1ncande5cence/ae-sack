cb72b918978716772b4e1a5c01e5c0a2f1715ad6
Zhechang Marinabay <zbz5352@psu.edu>
2025-10-26 23:18:55 -0400

    sudo u2 ready

---
+++ scripts/sudo/u2_approve/copy_tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+
+# subgt generation
+cp $1/tools/subgt_addresslog_gen.py $2
+cp $1/subgt/sudo/subgt.json $2
+
+# oracle input
+cp -r $1/scripts/sudo/u2_approve/input $2
+
+# result analysis 
+cp $1/scripts/sudo/report.py $2
